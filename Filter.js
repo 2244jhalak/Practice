66ccd1720d6a4b0509568101f2a40f752eeba38b
Jhalak <stdjhalak@gmail.com>
2025-01-10 22:18:38 +0600

    filter

---
+++ Filter.js
@@ -0,0 +1,13 @@
+const modify = properties => {
+    const filter = properties.filter(property=> property.name === "Tandra");
+    return filter.map(fil=>fil.name)
+}
+const data = [
+  { name: "Tandra", age: 30 },
+  { name: "Jhalak", age: 25 }
+];
+const result = (modify(data))[0];
+const main = data.filter(Adata=>Adata.name===result);
+main[0].age=27;
+
+console.log(data);
