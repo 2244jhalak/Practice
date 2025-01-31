3db1bd2bf1b0bad3b25bf501eabe5cdaa64593d7
Jhalak <stdjhalak@gmail.com>
2025-01-31 21:26:08 +0600

    Celsius To Farenheit Convertor

---
+++ CelsiusToFarenheit.js
@@ -0,0 +1,7 @@
+const CelsiusToFahrenheit = (celsius) => {
+    return (celsius * 9/5) + 32;
+}
+
+console.log(CelsiusToFahrenheit(0));
+console.log(CelsiusToFahrenheit(25));
+console.log(CelsiusToFahrenheit(100));
