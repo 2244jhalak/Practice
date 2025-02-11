0f709faaeea866b2b55b2871f02ccdd1e89cfd19
Jhalak <stdjhalak@gmail.com>
2025-02-11 23:53:09 +0600

    FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+const FizzBuzz = (n) => {
+    for (let i = 1; i <= n; i++) {
+      console.log(
+        i % 3 === 0 && i % 5 === 0 ? "FizzBuzz" :
+        i % 3 === 0 ? "Fizz" :
+        i % 5 === 0 ? "Buzz" :
+        i
+      );
+    }
+};
+  
+FizzBuzz(15);
+  
