4de9daca2ee53b6225f21780392aad6692c1446b
Jhalak <stdjhalak@gmail.com>
2025-02-25 23:55:38 +0600

    New Mission

--- Mission.js
+++ Mission.js
@@ -1,3 +1,4 @@
 const mission = "Tomorrow I will start my Javascript recap mission";
 const reality = "Still I couldn't start my mission";
-console.log(reality);
+const newMission = "I have started my new full stack project";
+console.log(newMission);
